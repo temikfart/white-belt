8cf3357753d14c63e4393e6329f10b717c325cff
temikfart <fartygin.a@phystech.edu>
2021-11-17 23:56:15 +0300

    w4(exceptions): simplier solution for task 4.

--- week-4/exceptions/4.cpp
+++ week-4/exceptions/4.cpp
@@ -2,25 +2,18 @@
 
 using namespace std;
 
-/*
 string AskTimeServer() {
 //  throw system_error(error_code());
-  throw invalid_argument("");
+//  throw invalid_argument("");
   return "01:00:00";
 }
-*/
 
 class TimeServer {
 public:
   string GetCurrentTime() {
-    string tmp = last_fetched_time;
     try {
       last_fetched_time = AskTimeServer();
-    } catch (system_error const & ex) {
-      last_fetched_time = tmp;
-    } catch (exception & e) {
-      throw;
-    }
+    } catch (const system_error&) {}
     return last_fetched_time;
   }
 private:
